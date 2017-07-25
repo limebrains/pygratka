077c6133d4ee6f41fc744ab6e81261d0c1389090
Jan Baryla <jan.baryla@limebrains.com>
2017-07-25 15:59:58 +0200

    Initial commit

---
+++ example.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import logging
+
+log = logging.getLogger(__file__)
+
+SCRAPE_LIMIT = os.environ.get('SCRAPE_LIMIT', None)
+
+if __name__ == '__main__':
+    pass
